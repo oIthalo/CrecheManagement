9eb7566ffd92ed0aec25ec3da4fb174fe379766e
oIthalo <ithalobarreto333@gmail.com>
2026-04-04 15:51:20 -0300

    refact: removendo idade do registro de estudante

--- CrecheManagement.Domain/Requests/RegisterStudentRequest.cs
+++ CrecheManagement.Domain/Requests/RegisterStudentRequest.cs
@@ -8,7 +8,6 @@ public class RegisterStudentRequest
     public string Name { get; set; }
     public string CPF { get; set; }
     public string? ContactNumber { get; set; }
-    public int Age { get; set; }
     public DateTime BirthDate { get; set; }
     public EGender Gender { get; set; }
     public List<IFormFile> Documents { get; set; }
